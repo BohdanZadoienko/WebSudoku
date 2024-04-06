609e52c17d79b2d25dedd07efc06b5f0ac9b9195
Bohdan Zadoienko <zadoienko@dexor.com>
2024-04-07 00:47:43 +0200

    feat: different colors for sub grids, active color rows/col

--- WebSudoku/obj/Debug/net8.0/WebSudoku.AssemblyInfo.cs
+++ WebSudoku/obj/Debug/net8.0/WebSudoku.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebSudoku")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d729b60acb86d7891820f38f2a77164fc43149a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebSudoku")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebSudoku")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
